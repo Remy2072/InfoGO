56ed1418013fc013ababa9fdefc8d0da105113a9
Remy2072 <remyd03@icloud.com>
2023-06-03 00:43:34 +0200

    login activated

--- src/Components/Rapports/Rapports.jsx
+++ src/Components/Rapports/Rapports.jsx
@@ -11,34 +11,7 @@ function Rapports() {
                   <div className="rapports__container-item-wrapper">
                     <h3>Architectuur</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
-                    <time>Aangemaakt op: 12-03-2023</time>
-                  </div>
-              </div>
-
-              <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
-                  <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
-                    <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
-                    <time>Aangemaakt op: 13-03-2023</time>
-                  </div>
-              </div>
-
-              <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
-                  <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
-                    <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
-                    <time>Aangemaakt op: 14-03-2023</time>
-                  </div>
-              </div>
-
-              <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
-                  <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
-                    <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
-                    <time>Aangemaakt op: 15-03-2023</time>
+                    <time>Aangemaakt op: 17-03-2023</time>
                   </div>
               </div>
 
@@ -56,7 +29,34 @@ function Rapports() {
                   <div className="rapports__container-item-wrapper">
                     <h3>Architectuur</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
-                    <time>Aangemaakt op: 17-03-2023</time>
+                    <time>Aangemaakt op: 15-03-2023</time>
+                  </div>
+              </div>
+
+              <div className="rapports__container-item">
+                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <div className="rapports__container-item-wrapper">
+                    <h3>Architectuur</h3>
+                    <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
+                    <time>Aangemaakt op: 14-03-2023</time>
+                  </div>
+              </div>
+
+              <div className="rapports__container-item">
+                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <div className="rapports__container-item-wrapper">
+                    <h3>Architectuur</h3>
+                    <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
+                    <time>Aangemaakt op: 13-03-2023</time>
+                  </div>
+              </div>
+
+              <div className="rapports__container-item">
+                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <div className="rapports__container-item-wrapper">
+                    <h3>Architectuur</h3>
+                    <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
+                    <time>Aangemaakt op: 12-03-2023</time>
                   </div>
               </div>
             </div>
