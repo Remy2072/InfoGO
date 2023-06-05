9309b4e0788ddac81c0c29d14de07e031c25aa06
Remy2072 <remyd03@icloud.com>
2023-06-05 12:59:19 +0200

    custom images added

--- src/Components/OverviewLesson/OverviewLesson.jsx
+++ src/Components/OverviewLesson/OverviewLesson.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import "./OverviewLesson.scss"
 import foto from "../../Assets/Rijksmuseum.jpg";
+import foto2 from "../../Assets/Arena.png";
+import foto3 from "../../Assets/Dam.png";
+import foto4 from "../../Assets/Mediacollege.png";
+import foto5 from "../../Assets/Nemo.png";
+import foto6 from "../../Assets/Rai.png";
+import foto7 from "../../Assets/Sloterdijk.png";
+import foto8 from "../../Assets/Tower.png";
+import foto9 from "../../Assets/Rembrandtplein.png";
 
 
 function OverviewLesson() {
@@ -9,63 +17,63 @@ function OverviewLesson() {
       <div className='overviewLesson__container'>
         <div className='overviewLesson__container-item'>
           <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <h3>Rijksmuseum</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 18-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto2} alt="Arena" />
+          <h3>Johan Cruijf Arena</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 19-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto3} alt="Dam" />
+          <h3>Dam</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 20-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img  src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img  src={foto4} alt="Mediacollege" />
+          <h3>Mediacollege</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 21-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto5} alt="Nemo" />
+          <h3>Nemo</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 22-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto6} alt="Rai" />
+          <h3>Rai</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 23-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto7} alt="Sloterdijk" />
+          <h3>Sloterdijk</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op:24-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto8} alt="Tower" />
+          <h3>Amsterdam Toren</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 25-03-2023</time>
         </div>
 
         <div className='overviewLesson__container-item'>
-          <img src={foto} alt="Rijksmuseum" />
-          <h3>Architectuur</h3>
+          <img src={foto9} alt="Rembrandtplein" />
+          <h3>Rembrandtplein</h3>
           <p>Architectuur is de kunst en wetenschap van het ontwerpen  van de gebouwde omgeving; steden, …</p>
           <time>Aangemaakt op: 26-03-2023</time>
         </div>
--- src/Components/Rapports/Rapports.jsx
+++ src/Components/Rapports/Rapports.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./Rapports.scss";
 import foto from "../../Assets/Rijksmuseum.jpg";
+import foto2 from "../../Assets/Arena.png";
+import foto3 from "../../Assets/Dam.png";
+import foto4 from "../../Assets/Mediacollege.png";
+import foto5 from "../../Assets/Nemo.png";
+import foto6 from "../../Assets/Rai.png";
+import foto7 from "../../Assets/Sloterdijk.png";
+import foto8 from "../../Assets/Tower.png";
+import foto9 from "../../Assets/Rembrandtplein.png";
 
 function Rapports() {
     return (
@@ -9,52 +17,52 @@ function Rapports() {
               <div className="rapports__container-item">
                   <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
                   <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
+                    <h3>Rijksmuseum</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
                     <time>Aangemaakt op: 17-03-2023</time>
                   </div>
               </div>
 
               <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <img className="rapports__container-item-tumb" src={foto2} alt="Arena" />
                   <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
+                    <h3>Johan Cruijf Arena</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
                     <time>Aangemaakt op: 16-03-2023</time>
                   </div>
               </div>
 
               <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <img className="rapports__container-item-tumb" src={foto3} alt="Dam" />
                   <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
+                    <h3>Dam</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
                     <time>Aangemaakt op: 15-03-2023</time>
                   </div>
               </div>
 
               <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <img className="rapports__container-item-tumb" src={foto4} alt="Mediacollege" />
                   <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
+                    <h3>Mediacollege</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
                     <time>Aangemaakt op: 14-03-2023</time>
                   </div>
               </div>
 
               <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <img className="rapports__container-item-tumb" src={foto5} alt="Nemo" />
                   <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
+                    <h3>Nemo</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
                     <time>Aangemaakt op: 13-03-2023</time>
                   </div>
               </div>
 
               <div className="rapports__container-item">
-                  <img className="rapports__container-item-tumb" src={foto} alt="Rijksmuseum" />
+                  <img className="rapports__container-item-tumb" src={foto6} alt="Rai" />
                   <div className="rapports__container-item-wrapper">
-                    <h3>Architectuur</h3>
+                    <h3>Rai</h3>
                     <p>Architectuur is de kunst en wetenschap van het ontwerpen van de gebouwde omgeving; inclusief steden, gebouwen, woningen, interieurs, tuinen, landschappen, meubelen of objecten</p>
                     <time>Aangemaakt op: 12-03-2023</time>
                   </div>
